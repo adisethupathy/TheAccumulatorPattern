24bd500b7dc7b2e83e530b2e4c7f95d13fc945a1
sethupa <sethupa@rose-hulman.edu>
2017-12-05 22:03:50 -0500

    m3

--- src/m3_graphical_accumulating.py
+++ src/m3_graphical_accumulating.py
@@ -9,8 +9,8 @@ Additionally, it emphasizes that you must
 before you can implement a solution to the problem in Python.
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Adi Sethupathy.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -27,7 +27,7 @@ import rosegraphics as rg
 # ----------------------------------------------------------------------
 def main():
     """ Calls the   TEST   functions in this module. """
-    run_test_draw_parallel_lines()
+    #run_test_draw_parallel_lines()
     run_test_draw_lines()
 
 
@@ -96,7 +96,7 @@ def draw_parallel_lines(n, point, length, window):
       :type window: rg.RoseWindow
     """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Tests have been written for you (above).
     #
     # CONSIDER using the ACCUMULATOR IN GRAPHICS pattern,
@@ -110,7 +110,14 @@ def draw_parallel_lines(n, point, length, window):
     ####################################################################
     # ------------------------------------------------------------------
 
-
+    for k in range(n+1):
+        start = rg.Point(point.x,point.y + 3*k)
+        end_x = point.x + length
+        end_y = point.y + 3*k
+        end = rg.Point(end_x, end_y)
+        line = rg.Line(start, end)
+        line.attach_to(window)
+        window.render()
 def run_test_draw_lines():
     """ Tests the   draw_lines  function. """
     print()
@@ -161,7 +168,7 @@ def draw_lines(n, point, window):
       :type window: rg.RoseWindow
     """
     # ------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #          Tests have been written for you (above).
     #
     # CONSIDER using the ACCUMULATOR IN GRAPHICS pattern,
@@ -174,6 +181,21 @@ def draw_lines(n, point, window):
     #          ** FIRST DO A CONCRETE EXAMPLE BY HAND! **
     ####################################################################
     # ------------------------------------------------------------------
+
+    for k in range(n+1):
+        pY = (point.y - 100)+(200/n)*k
+        start_x = point.x
+        start_y = pY
+        end_x = point.x + 100
+        end_y = pY
+        start = rg.Point(start_x, start_y)
+        end = rg.Point(end_x,end_y)
+
+        line = rg.Line(start, end)
+        line.attach_to(window)
+        window.render()
+
+
 
 
 # ----------------------------------------------------------------------
